b3e5ff7500841b1585dee891c0d9f6a7abaff294
Ad1thya-R <adithya.ravi@hotmail.com>
2022-09-17 22:16:32 +0200

    heroku test

--- src/components/aboutus/aboutus.jsx
+++ src/components/aboutus/aboutus.jsx
@@ -5,7 +5,7 @@ import Header from "../misc/navbar"
 import PersonCard from "./personcard"
 
 import Levente from "../../Images/levente.jpg"
-import ErikDz from "../../Images/erikdz.jpg"
+import AdiRav from "../../Images/Adithya.png"
 import Fryderyk from "../../Images/fryderyk.jpg"
 
 
@@ -17,20 +17,20 @@ function AboutUs(){
                 <Container className="align-and-center" style={{justifyContent:"space-around", padding:"100px 0 200px 0", flexWrap:"wrap"}}>
                     <PersonCard image={Levente}
                     name={"Levente Ludányi"}
-                    title={"Co-Founder - CTO"}
-                    description={"Student of Mathematics and Computer Science at École Polytechnique de Paris. Silver medalist at the International Physics Olympiad, Head of Finance at Future: Hungary and President of the Algorithmic Trading Society at École Polytechnique "}
+                    title={"Co-Founder"}
+                    description={"Student of Mathematics and Computer Science at École Polytechnique Paris. Silver medalist at the International Physics Olympiad and youngest quantitative researcher at WorldQuant LLC. In charge of: Product strategy, development and DeFi"}
                     personalsite={"ludanyilevente.com"}
                      />
-                    <PersonCard image={ErikDz}
-                    name={"Erik Dziekonski Bautista"}
-                    title={"Co-Founder - CTO"}
-                    description={<p>Student of Mathematics and Computer Science at École Polytechnique de Paris. Previous founder of <a style={{color:"white"}} href="https://www.youtube.com/watch?v=x52Z6FzyzOA">EasySend</a> and creator of <a style={{color:"white"}} href="src/components/aboutus/aboutus">PapersByTopic</a>. Backend manager of MEDICMAT. Discovered the CVE-2018-20448 exploit. </p>}
-                    personalsite={"erikdziekonski.com"}
+                    <PersonCard image={AdiRav}
+                    name={"Adithya Ravichandran"}
+                    title={"Co-Founder"}
+                    description={<p>Student of Mathematics and Computer Science at École Polytechnique Paris. Previous founder of LUXMUN and winner of multiple hackathons from FinTech to Game Dev. In charge of: Operations, Finances, Product development</p>}
+                    personalsite={"adithya.eu"}
                      />
                     <PersonCard image={Fryderyk}
                     name={"Fryderyk Sitnik"}
-                    title={"Co-Founder - COO"}
-                    description={"Student of Mathematics and Computer Science at École Polytechnique de Paris. Founder and President of the Board of Directors of the TeenCrunch Foundation, a non-profit startup accelerator for youth in Poland. Organized two batches of his accelerator with 9 startups in total."}
+                    title={"Co-Founder"}
+                    description={"Student of Mathematics and Computer Science at École Polytechnique Paris. Founder and President of the Board of Directors of the TeenCrunch Foundation, a non-profit startup accelerator for youth in Poland. In charge of: Business strategy, pitching, sales and investor relations"}
                     personalsite={""}
                      />
                 </Container>
